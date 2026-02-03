0ff36bdd4f133cfc6121d4eb3b1ab54a765daf0a
majanoca <maycon.janoca@gmail.com>
2026-02-03 20:59:16 -0300

    prazos

---
+++ js/analitico.js
@@ -0,0 +1,5 @@
+const file1 = document.querySelector("#file1");
+const file2 = document.querySelector("#file2");
+const file3 = document.querySelector("#file3");
+const file4 = document.querySelector("#file4");
+function lerArquivos() {}
