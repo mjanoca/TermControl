5659c5a6c3ccb2f003c36f43302da38f6c4eb1a4
majanoca <maycon.janoca@gmail.com>
2026-02-03 22:05:06 -0300

    inserindo async awai na função mesclar

--- js/analitico.js
+++ js/analitico.js
@@ -4,21 +4,23 @@ let totalDeArquivosCarregados = null;
 
 carregarArquivos();
 
-btnMesclar.addEventListener("click", (e) => {
-  file1.addEventListener("change", (e) => {
+btnMesclar.addEventListener("click", mesclar());
+
+async function mesclar() {
+  await file1.addEventListener("change", (e) => {
     for (let i = 0; i <= totalDeArquivosCarregados - 1; i++) {
       let file = e.target.files[i];
       if (file) {
         let reader = new FileReader();
         reader.addEventListener("load", (e) => {
           let result = e.target.result;
-          console.log(result.match(/.{11}BR/g));
+          console.log(...result.match(/.{11}BR/g));
         });
         reader.readAsText(file);
       }
     }
   });
-});
+}
 
 function carregarArquivos() {
   file1.addEventListener("change", (e) => {
