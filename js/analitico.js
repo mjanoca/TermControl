1f31ee542177bbbe576b362e29c554bcaaa921fc
majanoca <maycon.janoca@gmail.com>
2026-02-03 21:55:05 -0300

    Criação da leitura de varios arquivos de prazos

--- js/analitico.js
+++ js/analitico.js
@@ -1,5 +1,28 @@
 const file1 = document.querySelector("#file1");
-const file2 = document.querySelector("#file2");
-const file3 = document.querySelector("#file3");
-const file4 = document.querySelector("#file4");
-function lerArquivos() {}
+const btnMesclar = document.querySelector("#btn-mesclar");
+let totalDeArquivosCarregados = null;
+
+carregarArquivos();
+
+btnMesclar.addEventListener("click", (e) => {
+  file1.addEventListener("change", (e) => {
+    for (let i = 0; i <= totalDeArquivosCarregados - 1; i++) {
+      let file = e.target.files[i];
+      if (file) {
+        let reader = new FileReader();
+        reader.addEventListener("load", (e) => {
+          let result = e.target.result;
+          console.log(result.match(/.{11}BR/g));
+        });
+        reader.readAsText(file);
+      }
+    }
+  });
+});
+
+function carregarArquivos() {
+  file1.addEventListener("change", (e) => {
+    const selectFiles = e.target.files.length;
+    totalDeArquivosCarregados = Number(selectFiles);
+  });
+}
